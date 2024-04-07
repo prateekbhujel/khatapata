cdd334d26830d59fc4f1d737e1ab999c7678136c
Pratik Bhujel <prateekbhujelpb@gmail.com>
2024-04-07 12:50:41 +0545

    Update #5 -- 'Compling and fixing th issue of, login admin redirect and home redirect'

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -2,16 +2,39 @@
 
 namespace App\Http\Middleware;
 
+use Closure;
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
 use Illuminate\Http\Request;
 
 class Authenticate extends Middleware
 {
+
+    private $guards = [];
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @param  string[]  ...$guards
+     * @return mixed
+     *
+     * @throws \Illuminate\Auth\AuthenticationException
+     */
+    public function handle($request, Closure $next, ...$guards)
+    {
+        $this->guards = $guards;
+
+        $this->authenticate($request, $guards);
+
+        return $next($request);
+    }
+
     /**
      * Get the path the user should be redirected to when they are not authenticated.
      */
     protected function redirectTo(Request $request): ?string
     {
-        return $request->expectsJson() ? null : route('login');
+
+        return $request->expectsJson() ? null : (in_array('cms', $this->guards) ? route('admin.login.show') : route('login'));
     }
 }
