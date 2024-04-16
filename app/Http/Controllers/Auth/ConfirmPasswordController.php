84dcb4ad837a11f64f5313e36db8e5f230d20038
Pratik Bhujel <prateekbhujelpb@gmail.com>
2024-04-16 20:00:26 +0545

    Fixed Redirect Issue when logging in and Siningup

--- app/Http/Controllers/Auth/ConfirmPasswordController.php
+++ app/Http/Controllers/Auth/ConfirmPasswordController.php
@@ -25,7 +25,7 @@ class ConfirmPasswordController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/users/dashboard';
 
     /**
      * Create a new controller instance.
--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/users/dashboard';
+    protected $redirectTo = '/user/dashboard';
 
     /**
      * Create a new controller instance.
